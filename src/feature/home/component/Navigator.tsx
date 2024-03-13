29fa840e315c752613abd728779a8ad793a8cdaf
yeolsss <yeol8810@gmail.com>
2024-03-13 14:54:15 +0900

    navigator에 hover 추ㅏ

--- src/feature/home/component/Navigator.tsx
+++ src/feature/home/component/Navigator.tsx
@@ -36,39 +36,51 @@ const Navigator = () => {
       <nav className="fixed left-[10rem] top-[15rem] z-20 flex w-[10rem] flex-col max-sm:hidden">
         <ul className="flex flex-col gap-10">
           <li className="cursor-pointer" onClick={() => scrollToSection()}>
-            <span className="text-[2.4rem]">Main</span>
+            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+              Main
+            </span>
           </li>
 
           <li
             className="cursor-pointer"
             onClick={() => scrollToSection(introductionRef)}
           >
-            <span className="text-[2.4rem]">Introduction</span>
+            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+              Introduction
+            </span>
           </li>
 
           <li
             className="cursor-pointer"
             onClick={() => scrollToSection(skillsRef)}
           >
-            <span className="text-[2.4rem]">Skills</span>
+            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+              Skills
+            </span>
           </li>
           <li
             className="cursor-pointer"
             onClick={() => scrollToSection(careerRef)}
           >
-            <span className="text-[2.4rem]">Career</span>
+            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+              Career
+            </span>
           </li>
           <li
             className="cursor-pointer"
             onClick={() => scrollToSection(educationRef)}
           >
-            <span className="text-[2.4rem]">Education</span>
+            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+              Education
+            </span>
           </li>
           <li
             className="cursor-pointer"
             onClick={() => scrollToSection(trainingRef)}
           >
-            <span className="text-[2.4rem]">Training</span>
+            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+              Training
+            </span>
           </li>
         </ul>
       </nav>
