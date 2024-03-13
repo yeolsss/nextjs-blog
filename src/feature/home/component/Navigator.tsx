ff97d7495fcb6097b5e60cc4238d3371355d632f
yeolsss <yeol8810@gmail.com>
2024-03-13 15:39:22 +0900

    section에 맞는 navigator 보더 추가

--- src/feature/home/component/Navigator.tsx
+++ src/feature/home/component/Navigator.tsx
@@ -2,7 +2,13 @@ import { useContext, useEffect, useState } from 'react';
 import { ScrollContext } from '@/feature/home/context/ScrollObserver';
 import { PagePathContext } from '@/feature/home/context/PagePath';
 
+interface SectionRef {
+  ref: React.RefObject<HTMLElement>;
+  id: string;
+}
+
 const Navigator = () => {
+  const [activeSection, setActiveSection] = useState<string>('');
   const [isVisible, setIsVisible] = useState<boolean>(false);
   const {
     landingRef,
@@ -31,12 +37,64 @@ const Navigator = () => {
     return () => window.removeEventListener('scroll', checkVisible);
   }, [introductionRef, scrollY]);
 
+  useEffect(() => {
+    const sections: SectionRef[] = [
+      { ref: landingRef, id: 'landing' },
+      { ref: introductionRef, id: 'introduction' },
+      { ref: careerRef, id: 'career' },
+      { ref: skillsRef, id: 'skills' },
+      { ref: educationRef, id: 'education' },
+      { ref: trainingRef, id: 'training' },
+    ];
+
+    const handleScroll = () => {
+      const viewportCenter = window.innerHeight / 2 + window.scrollY;
+
+      const currentSectionId = sections.reduce(
+        (acc, section) => {
+          if (section.ref.current) {
+            const sectionTop = section.ref.current.offsetTop;
+            const sectionHeight = section.ref.current.offsetHeight;
+            const sectionCenter = sectionTop + sectionHeight / 2;
+
+            if (
+              !acc ||
+              Math.abs(viewportCenter - sectionCenter) <
+                Math.abs(viewportCenter - (acc.center || 0))
+            ) {
+              return { id: section.id, center: sectionCenter };
+            }
+          }
+          return acc;
+        },
+        { id: '', center: 0 },
+      ).id;
+
+      setActiveSection(currentSectionId);
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [
+    scrollY,
+    landingRef,
+    introductionRef,
+    careerRef,
+    skillsRef,
+    educationRef,
+    trainingRef,
+  ]);
+
   return (
     isVisible && (
       <nav className="fixed left-[10rem] top-[15rem] z-20 flex w-[10rem] flex-col max-sm:hidden">
         <ul className="flex flex-col gap-10">
           <li className="cursor-pointer" onClick={() => scrollToSection()}>
-            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+            <span
+              className={`text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor  ${activeSection === 'landing' ? 'border-b-2 border-solid border-b-accentColor' : ''}`}
+            >
               Main
             </span>
           </li>
@@ -45,7 +103,9 @@ const Navigator = () => {
             className="cursor-pointer"
             onClick={() => scrollToSection(introductionRef)}
           >
-            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+            <span
+              className={`text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor  ${activeSection === 'introduction' ? 'border-b-2 border-solid border-b-accentColor' : ''}`}
+            >
               Introduction
             </span>
           </li>
@@ -54,7 +114,9 @@ const Navigator = () => {
             className="cursor-pointer"
             onClick={() => scrollToSection(skillsRef)}
           >
-            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+            <span
+              className={`text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor  ${activeSection === 'skills' ? 'border-b-2 border-solid border-b-accentColor' : ''}`}
+            >
               Skills
             </span>
           </li>
@@ -62,7 +124,9 @@ const Navigator = () => {
             className="cursor-pointer"
             onClick={() => scrollToSection(careerRef)}
           >
-            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+            <span
+              className={`text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor  ${activeSection === 'career' ? 'border-b-2 border-solid border-b-accentColor' : ''}`}
+            >
               Career
             </span>
           </li>
@@ -70,7 +134,9 @@ const Navigator = () => {
             className="cursor-pointer"
             onClick={() => scrollToSection(educationRef)}
           >
-            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+            <span
+              className={`text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor  ${activeSection === 'education' ? 'border-b-2 border-solid border-b-accentColor' : ''}`}
+            >
               Education
             </span>
           </li>
@@ -78,7 +144,9 @@ const Navigator = () => {
             className="cursor-pointer"
             onClick={() => scrollToSection(trainingRef)}
           >
-            <span className="text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor">
+            <span
+              className={`text-[2.4rem] hover:border-b-2 hover:border-solid hover:border-b-accentColor  ${activeSection === 'training' ? 'border-b-2 border-solid border-b-accentColor' : ''}`}
+            >
               Training
             </span>
           </li>
